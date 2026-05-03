ad7eb187d315ebcc174e1e32c019596ad0cabcc8
yuyeming0115 <1798966227@qq.com>
2026-05-03 09:39:28 +0800

    精简同步文件结构

--- 参考模板/MMY-Tauri-Template/src/i18n/index.ts
+++
@@ -1,9 +0,0 @@
-import { createI18n } from 'vue-i18n'
-import zh from './zh'
-import en from './en'
-
-export const i18n = createI18n({
-  legacy: false,
-  locale: 'zh',
-  messages: { zh, en },
-})
